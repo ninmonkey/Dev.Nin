7a7b981691fb244b46f35b657e2785cbd9e8ddac
Jake Bolton <ninmonkeys@gmail.com>
2020-11-08 10:45:06 -0600

    Initial Commit: PrintTableTemplate

---
+++ Dev.Nin.psm1
@@ -0,0 +1,79 @@
+﻿$formatData = @(
+)
+
+foreach ($typeName in $formatData) {
+    $FileName = ("{0}\public\FormatData\nin-{1}.ps1xml" -f $psscriptroot, $typeName)
+    if (Test-Path $FileName ) {
+        Update-FormatData -PrependPath $FileName
+        Write-Verbose "Imported: FormatData: [$TypeName] $FileName"
+    } else {
+        Write-Error "Import: failed: FormatData: [$TypeName]  $FileName"
+    }
+}
+
+$private = @(
+
+)
+
+foreach ($file in $private) {
+    if (Test-Path ("{0}\private\{1}.ps1" -f $psscriptroot, $file)) {
+    } else {
+        Write-Error "Import: failed: private: $File"
+    }
+    . ("{0}\private\{1}.ps1" -f $psscriptroot, $file)
+}
+
+$public_NativeWrapper = @(
+
+)
+foreach ($file in $public_NativeWrapper) {
+    if (Test-Path ("{0}\public\native_wrapper\{1}.ps1" -f $psscriptroot, $file)) {
+    } else {
+        Write-Error "Import: failed: public\native_wrapper: $File"
+    }
+    . ("{0}\public\native_wrapper\{1}.ps1" -f $psscriptroot, $file)
+
+}
+
+Export-ModuleMember -Function $public_NativeWrapper
+
+$completer = @(
+
+)
+
+foreach ($file in $completer) {
+    if (Test-Path ("{0}\public\completer\{1}.ps1" -f $psscriptroot, $file)) {
+    } else {
+        Write-Error "Import: failed: completer: $File"
+    }
+    . ("{0}\public\completer\{1}.ps1" -f $psscriptroot, $file)
+}
+
+Export-ModuleMember -Function $completer
+
+$public = @(
+    'Dev-PrintTableTemplate'
+)
+
+foreach ($file in $public) {
+    if (Test-Path ("{0}\public\{1}.ps1" -f $psscriptroot, $file)) {
+    } else {
+        Write-Error "Import: failed: public: $File"
+    }
+    . ("{0}\public\{1}.ps1" -f $psscriptroot, $file)
+}
+
+$functionsToExport = @(
+    'Dev-PrintTableTemplate'
+)
+Export-ModuleMember -Function $functionsToExport
+
+if ($true) {
+
+    # New-Alias -ea 'Ignore' 'Docs' -Value 'Get-Docs' -Description 'Jump to docs by language'
+    $aliasesToExport = @(
+        # 'docs'
+    )
+
+    Export-ModuleMember -Alias $aliasesToExport
+}
---
+++ public/Dev-PrintTableTemplate.ps1
@@ -0,0 +1,96 @@
+﻿1, 127, 128, 255, 256 | hex | Join-String -sep ' '
+
+hr
+1, 127, 128, 255, 256 | hex | Join-String { '{0,10}' -f $_ }
+1, 127, 128, 255, 256 | hex | Join-String { '{0,-10}' -f $_ }
+
+
+function Dev-PrintTableTemplate {
+    <#
+    .synopsis
+        debug printing to compare other function against
+    .example
+        PS>
+        Table -NumColumns 15
+        Table -FillRemainingWidth -MinColWidth 10
+        Table -MinColWidth 30 -FillRemainingWidth
+
+        #output
+        |xxxx|xxxx|xxxx|xxxx|xxxx|xxxx|xxxx|xxxx|xxxx|xxxx|xxxx|xxxx|xxxx|xxxx|xxxx|
+        |xxxxxxxxxx|xxxxxxxxxx|xxxxxxxxxx|xxxxxxxxxx|xxxxxxxxxx|xxxxxxxxxx|
+        |xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx|xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx|
+    #>
+    [alias('Table')]
+    param(
+        [Parameter(
+            Position = 0,
+            HelpMessage = 'Min width for all columns')]
+        [uint]$MinColWidth = 4,
+
+        [Parameter(
+            Position = 0,
+            HelpMessage = 'Total number of columns per column')]
+        [uint]$NumColumns = 4,
+
+        [Parameter(
+            Position = 0,
+            HelpMessage = 'limit width of table')]
+        [uint]$MaxConsoleWidth = [console]::WindowWidth,
+
+        [Parameter(HelpMessage = "Fill remaining width with columns")][switch]$FillRemainingWidth
+
+
+    )
+    # [uint]$MinColWidth = 4
+    # [uint]$NumColumns = 3
+    # [uint]$MaxConsoleWidth = [console]::WindowWidth
+
+    $Template = @{}
+    $Template.RowPrefix = '|'
+    $Template.RowSuffix = '|'
+    $Template.Column = 'x'
+    $Template.ColumnSeperator = '|'
+
+    $splat_TableJoinString = @{
+        Separator    = $Template.ColumnSeperator
+        OutputSuffix = $Template.RowSuffix
+        OutputPrefix = $Template.RowPrefix
+    }
+
+    if (! $FillRemainingWidth ) {
+
+        1..$NumColumns | ForEach-Object {
+            'x' * $MinColWidth -join ''
+        } | Join-String @splat_TableJoinString
+        return
+    }
+
+    # calc dynamic number of columns
+    $ColPaddingCount = 2
+    $PotentialColumnCount = 0 + [math]::Floor( $MaxConsoleWidth / $MinColWidth )
+    Write-Debug "Potential: $PotentialColumnCount"
+    # $TotalCharWidth = $MaxConsoleWidth
+    do {
+        # refactor: It could attempt to generate a string then test the .Length
+        $ActualColumnCount = $PotentialColumnCount
+        $TotalCharWidth = 2 # prefix/suffix
+        $TotalCharWidth += 1 * $PotentialColumnCount # joiners
+        $TotalCharWidth += $MinColWidth * 1 * $PotentialColumnCount # inners
+        $PotentialColumnCount -= 1
+        Write-Debug "Total: $TotalCharWidth"
+    } while ( $TotalCharWidth -ge $MaxConsoleWidth )
+    Write-Debug "Total: $TotalCharWidth /w $PotentialColumnCount"
+
+    Dev-PrintTableTemplate -MinColWidth $MinColWidth -NumColumns $ActualColumnCount
+}
+
+
+Dev-PrintTableTemplate
+Dev-PrintTableTemplate -MinColWidth 5 -FillRemainingWidth -Debug
+
+h1 'start'
+
+hr
+Table -NumColumns 15
+Table -FillRemainingWidth -MinColWidth 10
+Table -MinColWidth 30 -FillRemainingWidth
