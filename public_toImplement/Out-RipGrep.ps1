d946eaaf948d4b70bada71867a9dc84e428ab82c
Jake Bolton <ninmonkeys@gmail.com>
2020-12-29 09:16:56 -0600

    new: GetPropSelected

--- public_toImplement/Out-RipGrep.ps1
+++ public_toImplement/Out-RipGrep.ps1
@@ -2,5 +2,8 @@ function Out-RipGrep {
     <#
     .synopsis
         should be able to pipe results to open in VS Code or clickable console file urls
+
+        see integration with 'Fzf'
+            <https://github.com/junegunn/fzf#3-interactive-ripgrep-integration>
     #>
 }
