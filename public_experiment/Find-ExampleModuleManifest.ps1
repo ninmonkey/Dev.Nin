0c1acb77670a8177fe2c6975f3dfd237c38a208d
Jake Bolton <ninmonkeys@gmail.com>
2021-08-22 18:35:51 -0500

    New: Find-ExampleModuleManifest and New-NinModuleManifest

---
+++ public_experiment/Find-ExampleModuleManifest.ps1
@@ -0,0 +1,101 @@
+if (! $DebugInlineToggle ) {
+    $experimentToExport.function += 'Find-ExampleModuleManifest'
+    $experimentToExport.alias += 'ModuleMetadata'
+}
+
+function Find-ExampleModuleManifest {
+    <#
+    .synopsis
+        boilerplate module creation
+    .description
+        .
+    .example
+        PS> Find-ExampleModuleManifest
+    .example
+        PS> Find-ExampleModuleManifest -PassThru | ft -AutoSize
+
+    .notes
+    .outputs
+
+    #>
+    [alias('ModuleMetadata')]
+    [CmdletBinding(PositionalBinding = $false)]
+    param(
+        # # Text to convert to a literal
+        [Alias('Name')]
+        [Parameter(Position = 0, ValueFromPipeline)]
+        [string[]]$ModuleName = '*',
+
+
+        [Alias('Mode')]
+        [ValidateSet('All', 'Tags')]
+        [Parameter()][string]$OutputMode = 'All',
+
+        [Parameter()][switch]$PassThru
+    )
+
+    begin {
+        function _modeAll {
+            # get module metadata
+            [cmdletbinding(PositionalBinding = $false)]
+            param()
+            Get-Module -Name $ModuleName | ForEach-Object {
+                $wantedProps = 'tags', 'name', 'description', 'ProjectUri', 'IconUri', 'LicenseUri', 'RepositorySourceLocation', 'Version', 'CompanyName', 'Copyright', @{
+                    'l' = 'TagsText' ; 'e' = {
+                        $_.Tags | Sort-Object -Unique | Join-String -sep ', ' -SingleQuote -op 'Tags: '
+                    }
+                }
+                $cur = $_
+                if ( $PassThru) {
+                    $cur
+                    return
+                }
+                if (! $PassThru ) {
+                    # $cur.Tags | Sort-Object -Unique | Join-String -sep ', ' -SingleQuote -op 'Tags: '
+
+                    # $cur | Join-String Tags -op 'tags: ' -sep ', '
+                    # hr
+                }
+                if (! $PassThru ) {
+                    $cur | Select-Object -Prop $wantedProps | Format-List
+                    hr
+                }
+            }
+        }
+        function _modeTags {
+            # get module metadata
+            [cmdletbinding(PositionalBinding = $false)]
+            param()
+            Write-Warning 'nyi; tag'
+        }
+
+    }
+    Process {
+        switch ($PSCmdlet.ParameterSetName) {
+            'Tags' {
+                _modeTags
+                break
+            }
+
+            'All' {
+                _modeAll
+                break
+            }
+            '__AllParameterSets' {
+                _modeAll
+                break
+            }
+
+            default {
+                throw "Unhandled ParameterSet: $($PSCmdlet.ParameterSetName)"
+            }
+        }
+    }
+    end {
+
+    }
+}
+
+if ($DebugInlineToggle) {
+    Find-ExampleModuleManifest pansies, classexplorer
+}
---
+++ public_experiment/New-NinModuleManifest.ps1
@@ -0,0 +1,123 @@
+$experimentToExport.function += 'New-NinModuleManifest'
+# $experimentToExport.alias += 'Re', 'ReLiteral'
+
+function New-NinModuleManifest {
+    <#
+    .synopsis
+        boilerplate module creation
+    .description
+        .
+    .example
+        $pattern = re 'something' -AsRipGrep
+        rg @('-i', $Pattern)
+    .notes
+    .outputs
+
+    #>
+    [alias('Re', 'ReLit')]
+    [CmdletBinding(PositionalBinding = $false, SupportsShouldProcess, ConfirmImpact = 'High')]
+    param(
+        # Text to convert to a literal
+        [Alias('Name')]
+        [Parameter(Mandatory, Position = 0, ValueFromPipeline)]
+        [string]$ModuleName,
+
+        # relative path to start in
+        [Alias('Path')]
+        [Parameter()][string]$BasePath = '.',
+
+        # Description Field
+        [Parameter()]
+        [string]$Description = 'description',
+
+        # Description Field
+        # suggest completer: VirtualTerminal, ANSI, EscapeSequences, Color, PSScriptAnalyzer, Dotnet, Class, Member, Reflection
+        [Parameter()]
+        [string[]]$Tags = @('')
+    )
+    begin {}
+    process {
+
+        $UserBasePath = Get-Item $BasePath
+        $ModuleBasePath = Join-Path $UserBasePath $ModuleName
+        if (Test-Path $ModuleBasePath) {
+            # todo: throw $PSCmdlet
+            Write-Error "Path already exists: '$ModuleBasePath'"
+            return
+        }
+        if ($PSCmdlet.ShouldProcess("'$ModuleBasePath'", 'Create Module Directory')) {
+            New-Item -Path $ModuleBasePath -ItemType Directory
+        }
+
+        # $ManifestPath = Join-Path $ModuleBasePath "$Module.psd1"
+
+        <#
+
+        UserBasePath = '.'
+        ManifestPath_FullName          C:\Users\cppmo_000\Documents\2021\Powershell\My_Github\micro.modules\C:\Users\cppmo_000\Documents\2021\Powershell\My_Github\micro.modules\business
+        ModuleBasePath                 C:\Users\cppmo_000\Documents\2021\Powershell\My_Github\micro.modules\business
+        ModuleBasePath: C:\Users\cppmo_000\Documents\2021\Powershell\My_Github\micro.modules\business
+        ModuleName                     business
+        MyInvocation                   System.Management.Automation.InvocationInfo
+        newModuleManifestSplat         {ProjectUri, Copyright, Description, ModuleVersion…}
+        null
+        PSBoundParameters              {}
+        PSCmdlet                       System.Management.Automation.PSScriptCmdlet
+        PSCommandPath
+        PSDebugContext                 System.Management.Automation.PSDebugContext
+        PSItem
+        PSScriptRoot
+        RootModulePath                 business.psm1
+        RootModulePath_FullName        C:\Users\cppmo_000\Documents\2021\Powershell\My_Github\micro.modules\C:\Users\cppmo_000\Documents\2021\Powershell\My_Github\micro.modules\business
+        Tags                           {}
+        true                           True
+        UserBasePath                   C:\Users\cppmo_000\Documents\2021\Powershell\My_Github\micro.modules
+
+
+
+        #>
+        $ManifestName = "${ModuleName}.psd1"
+        $RootModuleName = "${ModuleName}.psm1"
+        $ManifestPath_FullName = (Join-Path $ModuleBasePath $ManifestName)
+        $RootModulePath_FullName = (Join-Path $ModuleBasePath $ModuleBasePath)
+        $newModuleManifestSplat = @{
+            Path          = $ManifestName
+            RootModule    = $RootModuleName
+            Author        = 'Jake Bolton' # (github.com/ninmonkey/dotfiles_git)'
+            ModuleVersion = '0.0.1'
+            Description   = $Description
+            Tags          = $Tags
+            ProjectUri    = 'https://github.com/ninmonkey/{0}' -f @($ModuleName)
+            LicenseUri    = 'https://github.com/ninmonkey/{0}/blob/master/LICENSE' -f @($ModuleName)
+            CompanyName   = 'ninmonkeys.com'
+            Copyright     = '(c){0} Jake Bolton. All rights reserved.' -f @( (Get-Date).Year )
+        }
+        if ([string]::IsNullOrWhiteSpace($Tags)) {
+            $newModuleManifestSplat.Remove('Tags')
+        }
+
+        if (Test-Path $ManifestPath_FullName) {
+            # todo: throw $PSCmdlet
+            Write-Error "Path already exists: '$ManifestPath_FullName'"
+            return
+        }
+        if (Test-Path $RootModulePath_FullName) {
+            # todo: throw $PSCmdlet
+            Write-Error "Path already exists: '$RootModulePath_FullName'"
+            return
+        }
+
+        # $newModuleManifestSplat | Format-Table
+
+        if ($PSCmdlet.ShouldProcess("'$ManifestPath_FullName'", 'Write New Manifest?')) {
+            # New-ModuleManifest @newModuleManifestSplat
+            # New-Item -Path $RootModulePath_FullName -ItemType File
+        }
+
+        Get-ChildItem . -Recurse
+
+        New-Text -fg orange 'NYI: Initialize git repo' | Join-String
+
+    }
+    end {}
+}
