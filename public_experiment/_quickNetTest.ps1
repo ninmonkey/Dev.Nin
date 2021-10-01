88f2731eeaf5d18194d61909b7a8d94391be2285
Jake Bolton <ninmonkeys@gmail.com>
2021-09-30 21:10:50 -0500

    New: NetworkTool🌎.SlowNetTest

---
+++ public_experiment/_quickNetTest.ps1
@@ -0,0 +1,28 @@
+$experimentToExport.function += @(
+    '_quickNetTest'
+)
+$experimentToExport.alias += @(
+    'PingQuick'
+    'NetworkTool🌎.SlowNetTest'
+)
+
+function _quickNetTest {
+    <#
+    .synopsis
+        when you don't need detailed stats, just a quick ping result
+    .link
+        Dev.Nin\NetworkTool🌎.SlowNetTest
+    #>
+    # quick 1 off test
+    [Alias('NetworkTool🌎.SlowNetTest', 'PingQuick')]
+    [CmdletBinding()]
+    param()
+    $isp_dns = @('205.171.2.25', '205.171.3.25')
+    $known_dns = @('1.1.1.1', '8.8.8.8')
+    $targets = @(
+        $isp_dns
+        $known_dns
+        'google.com', 'reddit.com', 'ninmonkeys.com', 'ninmonkey.com'
+    )
+    Test-Connection -TargetName $targets -ResolveDestination
+}
---
+++ public_experiment/_slowNetTest.ps1
@@ -0,0 +1,29 @@
+$experimentToExport.function += @(
+    '_slowNetTest'
+)
+$experimentToExport.alias += @(
+    'PingSlow'
+    'NetworkTool🌎.SlowNetTest'
+   
+)
+
+function _slowNetTest {
+    <#
+    .synopsis
+        when you don't need detailed stats, just a quick ping result
+    .link
+        Dev.Nin\NetworkTool🌎.QuickNetTest
+    #>
+    # quick 1 off test
+    [Alias('PingSlow', 'NetworkTool🌎.SlowNetTest')]
+    [CmdletBinding()]
+    param()
+    $isp_dns = @('205.171.2.25', '205.171.3.25')
+    $known_dns = @('1.1.1.1', '8.8.8.8')
+    $targets = @(
+        $isp_dns
+        $known_dns
+        'google.com', 'reddit.com', 'ninmonkeys.com', 'ninmonkey.com'
+    )
+    Test-Connection -TargetName $targets -ResolveDestination
+}
