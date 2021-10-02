d452a0712fa9278cd34a8b374b380ca257fd107a
Jake Bolton <ninmonkeys@gmail.com>
2021-10-02 15:49:06 -0500

    Update Write-Endcap.ps1

--- public_experiment/Write-Endcap.ps1
+++ public_experiment/Write-Endcap.ps1
@@ -17,46 +17,61 @@ function Write-Endcap {
     param(
         # Text array/list
         [Parameter(Mandatory, ValueFromPipeline)]
-        [string]$InputObject
+        [string]$InputObject,
+
+        # Label of region
+        [Alias('Text')]
+        [Parameter(Position = 0)]
+        [string]$Label
+
+
     ) 
     begin {
-        $Theme = 'advanced'
-        if ($Theme -eq 'simple') {
-            $color_fg = gi fg:\green
-            $color_bg = '#feab99'
-            $color_bg = 'seafoam'
-            $text_prefix = "<`n"
-            $text_suffix = "`n>`n"               
-        }
-        elseif ($Theme -eq 'advanced') {
-            $color_fg = gi fg:\green
-            $color_bg = '#feab99'
-            $color_bg = 'seafoam'
-            $text_prefix = @(
-                "<`n" | Write-color 'pink'
-            )
-            $text_suffix = "`n>`n"               
-        }
+        # $Theme = 'simple'
+        # if ($Theme -eq 'simple') {
+        #     $color_fg = gi fg:\green
+        #     $color_bg = '#feab99'
+        #     $color_bg = 'seafoam'
+        #     $text_prefix = "<`n"
+        #     $text_suffix = "`n>`n"               
+        # }
+        # elseif ($Theme -eq 'advanced') {
+        #     $color_fg = gi fg:\green
+        #     $color_bg = '#feab99'
+        #     $color_bg = 'LightSeaGreen'
+        #     $text_prefix = @(
+        #         "<`n" | Write-color 'pink'
+        #     ) -join ''
+        #     $text_suffix = "`n>`n"               
+        # }
+    }
+    process {
+        $label = 'gc test'
+        @(
+            write-color -t "<----- Start: $Label" -fg green4 -bg lightgreen
+            'stuff'
+            write-color -t "<-----   End: $Label" -fg green4 -bg lightgreen
+        )
     }
     end {
         #  "␤" "␀"        
         # except: "␣"
-        $joinStringSplat = @{
-            # DoubleQuote = $true
-            OutputPrefix = @(
-                "`n"
-                Write-TextColor -fg $color_fg -Text $text_prefix
-                "`n"
-            ) -join ''
-            OutputSuffix = @(
-                "`n"
-                Write-TextColor -fg $color_fg -Text $text_suffix
-                "`n"
-            ) -join ''
-            # $x = '#feab99'    
-        }
+        # $joinStringSplat = @{
+        #     # DoubleQuote = $true
+        #     OutputPrefix = @(
+        #         "`n"
+        #         $text_prefix | Write-TextColor -fg $color_fg
+        #         "`n"
+        #     ) -join ''
+        #     OutputSuffix = @(
+        #         "`n"
+        #         Write-TextColor -fg $color_fg -Text $text_suffix
+        #         "`n"
+        #     ) -join ''
+        #     # $x = '#feab99'    
+        # }
 
-        $InputObject | Join-String @joinStringSplat
+        # $InputObject | Join-String @joinStringSplat -ea break
     }
 }
 
