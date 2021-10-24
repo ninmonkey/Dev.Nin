99f1ae10f85eb57fb28e03c12893a98406833f9d
Jake Bolton <ninmonkeys@gmail.com>
2021-10-24 09:24:26 -0500

    Update Match-String.tests.ps1

--- public_experiment/Match-String.tests.ps1
+++ public_experiment/Match-String.tests.ps1
@@ -1,27 +1,33 @@
-#requires -modules @{ModuleName='Pester';ModuleVersion='5.0.0'}
-$SCRIPT:__PesterFunctionName = $myinvocation.MyCommand.Name.split('.')[0]
+# requires -modules @{ModuleName='Pester';ModuleVersion='5.0.0'}
+# $SCRIPT:__PesterFunctionName = $myinvocation.MyCommand.Name.split('.')[0]
 
-Describe "$__PesterFunctionName" -Tag Unit {
+Describe 'Match-String' -Tag Unit {
     BeforeAll {
         Import-Module Dev.Nin -Force
         # . $(Get-ChildItem -Path $PSScriptRoot/.. -Recurse -Filter "$__PesterFunctionName.ps1")
         # $Mocks = Resolve-Path "$PSScriptRoot/Mocks"
-        $ErrorActionPreference = 'Stop'
+        $ErrorActionPreference = 'stop'
     }
     It 'Runs without error' {
-        { 'foo' | . $__PesterFunctionName -Regex 'o' }
+        { 'foo' | Match-String -Regex 'o' }
         | Should -Not -Throw
     }
     Describe 'Basic Static' {
         BeforeAll {
             $Sample1 = 'a', '3', 'eaf'
         }
-        It 'Supports Null' {
-            $null | Where-String ''
-            $null | Match-String ''
-            $null | Should -Be $Null
-            $null | Where-Object { $_ -match '' }
-        }
+        # Describe 'Supports Null' {
+        #     <#
+        #         - Should It allow null without opt-ing in?
+        #             for dealing with piped in raw text, yes, allow should not throw
+        #         - if compared to an object, then, null should require an opt-in
+        #     #>
+        #     # It 'Null Param From Pipe' {
+        #     #     # $null | Match-String '.*'
+        #     #     # $null | Should -Be $Null
+        #     #     # $null | Where-Object { $_ -match '' }
+        #     # }
+        # }
         It 'Basic Static' {
             $Res1 = $Sample1 | Where-Object { $_ -match '\d+' }
             $Res1 = $Sample1 | Match-String { '\d+' }
@@ -31,4 +37,11 @@ Describe "$__PesterFunctionName" -Tag Unit {
         }
 
     }
+    # Describe 'NotMatching' {
+    #     It 'default' {
+    #         'a', '3' | Match-String '\d+'  #-ea break
+    #         | Should -Be '3'
+
+    #     }
+    # }
 }
