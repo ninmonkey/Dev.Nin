4ea7019a6e75088b16e85e3a79fdf319b9877d7b
Jake Bolton <ninmonkeys@gmail.com>
2021-10-26 12:03:58 -0500

    Find-ObjectProperty: prints interfaces, new param auto sorts for OGV

--- public_experiment/Find-Property.ps1
+++ public_experiment/Find-Property.ps1
@@ -30,11 +30,43 @@ function _enumerateProperty {
     [Alias('iterProp', 'Find-ObjectProperty')]
     [cmdletbinding()]
     param(
-        #
+        # any object
         [Parameter(Mandatory, Position = 0, ValueFromPipeline)]
-        [object]$InputObject
+        [object]$InputObject,
+
+        # preset column order, and to out-griview
+        [alias('oGv')]
+        [parameter()]
+        [switch]$OutGridView
     )
     process {
+        $meta = @{
+            # ImplementedInterfaces
+        }
+        $tinfo = $InputObject.GetType()
+        [ordered]@{
+            Name     = $tinfo.Name
+            TypeName = $tinfo | Format-TypeName -Brackets
+        } | Format-Dict
+        | Write-Information
+
+        $tinfo.ImplementedInterfaces | Format-TypeName -Brackets
+        | STR UL -Sort -Unique
+        | str Prefix 'ImplementedInterfaces: '
+        | Write-Information
+
+        if (! $OutGridView ) {
+            $InputObject.psobject.properties
+            return
+        }
+
+        $title_ogv = '{0} ⁞ {1}' -f @(
+            $tinfo | Format-TypeName -Brackets
+            $Tinfo.FullName -replace '^System\.', ''
+        )
         $InputObject.psobject.properties
+        | Select-Object Name, Value, TypeNameOfValue, * -ea ignore
+        | Sort-Object Name # TypeNameOfValue
+        | Out-GridView -PassThru -Title $title_ogv
     }
 }
