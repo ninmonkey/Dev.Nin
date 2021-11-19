dc724cc67654a2548ddef4c9a7e4c154c10e681d
Jake Bolton <ninmonkeys@gmail.com>
2021-11-19 14:34:29 -0600

    iter hash

--- public_experiment/New-HashtableFromObject.ps1
+++ public_experiment/New-HashtableFromObject.ps1
@@ -3,13 +3,109 @@
 if ( $experimentToExport ) {
     $experimentToExport.function += @(
         'New-HashtableFromObject'
+        'Select-HashtableKey'
     )
     $experimentToExport.alias += @(
         'To->Hashtable'
         'ConvertTo-Hashtable'
+        # 'Filter->Hashtable',
+     
     )
 }
 
+function Select-HashtableKey {
+    <#
+    .synopsis
+        Create a hashtble by filtering keys
+    .description
+       .
+    .outputs
+        [hashtable]
+    #>    
+    [Alias(
+        # 'Filter->Hashtable',
+        'Select-HashtableKey'
+    )]
+    [outputtype('hashtable')]
+    [CmdletBinding(PositionalBinding = $false)]
+    param(
+        # Source object
+        [Parameter(Mandatory, ValueFromPipeline, Position = 0)]
+        [hashtable]$InputObject,
+
+        #Docstring
+
+        [Parameter(Mandatory, Position = 1)]
+        [string[]]$IncludeRegex
+    )
+    begin {
+        <#
+        todo test case
+        
+                $a = @{'a' = 34; 'ze' = 55}
+                $b = @{'a' = 'good' ; q = 'bad'}
+                $c = [pscustomobject]$b | New-HashtableFromObject -IncludeProperty 'a'
+                hr
+                Join-Hashtable $a $c
+                hr
+                $c
+
+        #>
+        
+    }
+    process {
+        [hashtable]$newHash = @{}
+
+        # todo: refactor using AnyTrue or AnyFalse , maybe in Utility
+        $filteredKeys = $InputObject.Keys.clone() | Where-Object {
+            $curName = $_
+            foreach ($pattern in $IncludeRegex) {
+                if ($curName -match $Pattern) {
+                    $false; return;
+                }
+            }
+            $true; return;
+        }
+        'Original Keys {0}' -f @(
+            $InputObject.Keys | str csv -SingleQuote -Sort
+        ) | Write-Debug
+        'Filtered Keys: {0}' -f @(
+            $filteredKeys | str csv -SingleQuote -Sort
+        ) | Write-Debug
+        
+        $filteredKeys | ForEach-Object {
+            $curKey = $_            
+            $newHash[ $curKey ] = $InputObject[ $curKey ]            
+        }
+        $NewHash
+        return 
+
+        $UpdateHash.GetEnumerator() | ForEach-Object {
+            $anyMatch = 
+            $NewHash[ $_.Key ] = $_.Value
+        }
+        $NewHash
+
+        return 
+
+
+
+        # todo: refactor using AnyTrue or AnyFalse , maybe in Utility
+        $InputObject.keys.clone() | ForEach-Object {
+            $curKey = $_
+            $matchesAny = $IncludeRegex | Where-Object {
+                $curName = $_
+                foreach ($curRegex in $IncludeRegex) {
+                    if ($curKey -match $curRegex) {
+                        $true; return;
+                    }
+                }
+            }
+            "key '$curKey' matches any? $matchesAny" | Write-Debug
+        }
+    }
+
+}
 function New-HashtableFromObject {
     <#
     .synopsis
@@ -130,8 +226,8 @@ function New-HashtableFromObject {
 
 if (! $experimentToExport) {
     'HI'
-    Get-Date | New-HashtableFromObject -Debug -RegexInclude  'day', '.*year', 'kind' -ExcludeProperty '.*of.*'
-    $h = Get-Date | New-HashtableFromObject -Debug -RegexInclude  'day', '.*year', 'kind' -ExcludeProperty '.*of.*'
+    Get-Date | New-HashtableFromObject -Debug -RegexInclude 'day', '.*year', 'kind' -ExcludeProperty '.*of.*'
+    $h = Get-Date | New-HashtableFromObject -Debug -RegexInclude 'day', '.*year', 'kind' -ExcludeProperty '.*of.*'
     hr$
     $h
     hr
