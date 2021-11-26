9919c92730d85acfff4f17effd17a4b1a71def42
Jake Bolton <ninmonkeys@gmail.com>
2021-11-26 16:08:58 -0600

    testing format

---
+++ public_experiment/help/Get-NamespaceHelp.ps1
@@ -0,0 +1,82 @@
+#Requires -Version 7
+
+if ( $experimentToExport ) {
+    $experimentToExport.function += @(
+        'Get-NamespaceHelp'
+    )
+    $experimentToExport.alias += @(
+        'Help->Namespace'
+    )
+}
+
+function Get-NamespaceHelp {
+    [alias('Help->Namespace')]
+    param(
+        [Parameter(Position = 0, ValueFromPipeline)]
+        [string]$Namespace = 'system',
+
+        [Parameter()][int]$RelativeDepth = 2
+    )
+
+    # $namespace = 'system.management.automation'
+    function _renderName {
+        [string]$Item
+        if ($NoColor) {
+            ($Item)?.Name ?? $Item
+            return
+        }
+        $Item.Name
+    }
+    @(
+
+        $namespace | ForEach-Object { $_ -replace '^system\.', '' }
+        | str ul
+        Find-Type -Namespace $Namespace | ForEach-Object {
+            # _renderName $_.Name
+            $_.Name
+        }
+        | str ul -sort -Unique | Format-IndentText -Depth $RelativeDepth
+    ) | str nl 0
+}
+
+if (! $experimentToExport) {
+    # ...
+    Help->Namespace system.text
+    Help->Namespace system.text
+}
+function _disabled__refactor-Get-NamespaceHelp {
+    <#
+    .synopsis
+        quickly dump files in namespace, with some formatting
+
+    #>
+    [alias('Help->Namespace')]
+    param(
+        [Alias('Name')]
+        [Parameter(Position = 0, ValueFromPipeline)]
+        [string]$Namespace = 'system',
+
+        [Parameter()][int]$RelativeDepth = 2,
+
+        [Parameter()][switch]$NoColor
+    )
+
+    # $namespace = 'system.management.automation'
+    function _renderName {
+        [string]$Item
+        if ($NoColor) {
+            $Item.Name; return
+        }
+        $Item.Name
+    }
+
+    @(
+
+        $namespace | ForEach-Object { $_ -replace '^system\.', '' }
+        | str ul
+
+        Find-Type -Namespace $Namespace | ForEach-Object { _renderName -name $_ }
+        | str ul -sort -Unique | Format-IndentText -Depth $RelativeDepth
+    ) | str nl 0
+}
+
