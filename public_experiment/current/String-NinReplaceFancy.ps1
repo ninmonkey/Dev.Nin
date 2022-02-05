c433c8a93235f79d5428d4ffb5152c0cee396986
Jake Bolton <ninmonkeys@gmail.com>
2022-02-05 17:02:24 -0600

    reverting top level log of string replace

---
+++ public_experiment/current/String-NinReplaceFancy.ps1
@@ -0,0 +1,159 @@
+#Requires -Version 7
+
+if ( $experimentToExport ) {
+    $experimentToExport.function += @(
+        'String-NinReplaceFancy'
+    )
+    $experimentToExport.alias += @(
+        # ''
+    )
+}
+
+
+function String-NinReplaceFancy {
+    <#
+    .synopsis
+        sugar for "$x | %{ $_ -replace $regex, $replacement }"
+    .description
+       .not working correctly, revert to <200853751cff40505f6daf06f6e3abaf4c90277e>
+    .example
+          .
+    .outputs
+          [string
+    .link
+        Irregular
+    .link
+        Dev.Nin\Match-String
+    .link
+        Dev.Nin\Split-String
+    .link
+        Dev.Nin\Where-MatchAnyText
+    .link
+        Ninmonkey.Console\ConvertFrom-NumberedFilepath
+    .link
+        Dev.Nin\Remove-AnsiEscape
+
+    #>
+    [Alias(
+        # 'StrReplace'
+        # '-replace' # tab complete do;esn't initialize on prefix '-'
+    )]
+    [CmdletBinding()]
+    param(
+        # source text
+        [parameter(Mandatory, ValueFromPipeline)]
+        [string]$InputText,
+
+        # regex pattern
+        [Alias('Pattern')]
+        [AllowEmptyString()]
+        [Parameter(
+            ParameterSetName = 'BasicRegex',
+            Mandatory, Position = 0
+        )]
+        [string]$RegexPattern,
+
+        # replacement pattern, works with -Templates
+        [parameter(ParameterSetName = 'BasicRegex', Position = 1)]
+        [parameter(ParameterSetName = 'ReplaceTemplate', Position = 0)]
+        [AllowEmptyString()]
+        [string]$SubstitutionString = [string]::Empty,
+
+        # keeping the parameter set[s] undeclared for now, to simplify code
+        [Alias('Template')]
+        [parameter(Mandatory, ParameterSetName = 'ReplaceTemplate')]
+        [ValidateSet(
+            # [ArgumentCompletions(
+            'Markdown_Underscore', 'Markdown_InvertUnderscore',
+            'Markdown_EscapeSpace', 'Markdown_InvertEscapeSpace'
+        )]
+        [AllowEmptyString()]
+        [string]$ReplacementMode#  = [string]::Empty
+
+    )
+
+    # future: generate dynamically, using hashtable or yml/json config files
+    begin {
+        # default config
+        $orignalSubString = $DefaultSubstitutionString
+        switch ($PSCmdlet.ParameterSetName) {
+            'BasicRegex' {
+                break
+            }
+            'ReplaceTemplate' {
+                switch ($ReplacementMode) {
+                    'Markdown_Underscore' {
+                        $RegexPattern = '\s'
+                        $SubstitutionString = '_'
+                        break
+                    }
+                    'Markdown_InvertUnderscore' {
+                        $RegexPattern = '_'
+                        $SubstitutionString = ' '
+                        break
+                    }
+
+                    'Markdown_EscapeSpace' {
+                        $RegexPattern = '\s'
+                        $SubstitutionString = '%20'
+                        break
+                    }
+                    'Markdown_InvertEscapeSpace' {
+                        $RegexPattern = [regex]::escape( '%20' )
+                        $SubstitutionString = ' '
+                        break
+                    }
+                    default {
+                        throw "Unhandled template: $($ReplacementMode)"
+                    }
+
+                }
+                # break # was this bad?
+            }
+            default {
+                throw "Unhandled ParameterSet: $($PSCmdlet.ParameterSetName)"
+            }
+        }
+        # Markdown_Underscore
+        # $PSCmdlet.MyInvocation.BoundParameters
+        # $PSBoundParameters
+        # # Wait-Debugger
+
+        if ( $PSCmdlet.MyInvocation.BoundParameters.ContainsKey('SubstitutionString')) {
+            # warning this doesn't set correctly
+            $SubstitutionString = $orignalSubString
+            Write-Debug 'override, using users value'
+        }
+
+        Label 'regex' $RegexPattern | Write-Debug
+        Label 'substitute' $SubstitutionString | Write-Debug
+    }
+    process {
+        if ( $PSCmdlet.MyInvocation.BoundParameters.ContainsKey('SubstitutionString')) {
+            $SubstitutionString = $orignalSubString
+            Write-Debug 'override, using users value'
+        }
+        @{
+            Regex      = $RegexPattern
+            Substitute = $SubstitutionString
+        } | Format-Table | Write-Debug
+        switch ($PSCmdlet.ParameterSetName) {
+            'BasicRegex' {
+                $InputText | ForEach-Object { $_ -replace $RegexPattern, $SubstitutionString }
+            }
+            'ReplaceTemplate' {
+                $InputText | ForEach-Object { $_ -replace $RegexPattern, $SubstitutionString }
+            }
+            default {
+                throw "Unhandled ParameterSet: $($PSCmdlet.ParameterSetName)"
+            }
+        }
+    }
+    end {
+    }
+}
+
+
+if (! $experimentToExport) {
+    # ...
+}
