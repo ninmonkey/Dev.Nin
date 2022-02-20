e247685c3cca79cc5758fecd6942c2eadb4ae903
Jake Bolton <ninmonkeys@gmail.com>
2022-02-20 13:41:12 -0600

    Update minimal-ObjectTypes.ps1

--- public_experiment/inspect/minimal-ObjectTypes.ps1
+++ public_experiment/inspect/minimal-ObjectTypes.ps1
@@ -3,32 +3,30 @@ using namespace Management.Automation
 if ( $experimentToExport ) {
     $experimentToExport.function += @(
         'objTypes'
+        'objTypes_basic'
     )
     $experimentToExport.alias += @(
-        'Info->objTypes'
+        'Info->objTypes' # 'objTypes'
+        'Info->objTypes_basic' # 'objTypes'
     )
 }
 
-
-function objTypes {
-    <#
-    .example
-        , (Get-ChildItem .) | objTypes
-    #>
-    [cmdletbinding()]
+function objTypes_basic {
+    [CmdletBinding()]
+    [Alias('Info->objTypesBasic_basic')]
     param(
         [Parameter(Position = 0, ValueFromPipeline)]
         [object]$InputObject
     )
-
     process {
         $InputObject.GetType().Name
         $InputObject.PSTypeNames
         | ShortName
         | Join-String -sep ', '
-        | Join-String -op 'PSTypeNames: '
         | Join-String -sep ', '
+        | Join-String -op 'PSTypeNames: '
         | Format-IndentText
+
         if ($InputObject.count -gt 1) {
             $InputObject[0].GetType().Name
             | Join-String -op 'Child: '
@@ -42,4 +40,92 @@ function objTypes {
 
         }
     }
+}
+
+function objTypes {
+    <#
+    .synopsis
+        command "Dev.Nin\objTypes_basic" with colors
+    .notes
+        **NOT Performant** . Experimenting with dynamic expressions
+    .link
+        Dev.Nin\objTypes
+    .link
+        Dev.Nin\objTypes_basic
+    #>
+    [CmdletBinding()]
+    [Alias('Info->objTypes')]
+    param(
+        [Parameter(Position = 0, ValueFromPipeline)]
+        [object]$InputObject,
+
+        # extra options
+        [Parameter()][hashtable]$Options
+    )
+    begin {
+        # [hashtable]$ColorType = Join-Hashtable $ColorType ($Options.ColorType ?? @{})
+        [hashtable]$Config = @{
+            BaseDepth = 1
+        }
+        $Config = Join-Hashtable $Config ($Options ?? @{})
+        $Str = @{}
+
+        $Str.ColorDim = @(
+            '' | Write-color -fg (Color->Gray 30)
+        ) -join ''
+
+        $Str.PrefixPSTypenames = @(
+            'PSTypeNames: ' | Write-Color -fg (Color->Gray 50)
+            $PSStyle.Reset
+            # $Str.ColorDim
+            '' | Write-color -fg (Color->Gray 30)
+            # "${fg:clear}"
+            # $PSStyle.Foreground
+        ) -join ''
+        $Str.PrefixFirst = @(
+            'First: ' | Write-Color -fg (Color->Gray 50)
+            $PSStyle.Reset
+            # "${fg:clear}"
+            # $PSStyle.Foreground
+        ) -join ''
+
+
+    }
+    process {
+        $splatDent = @{
+            # IndentString = '  '
+            IndentString = '  '
+        }
+        $curDepth = $Config.BaseDepth
+
+        $InputObject.GetType().Name
+        $InputObject.PSTypeNames
+        | ShortName
+        | Join-String -sep ', '
+        | Join-String -op $Str.PrefixPSTypeNames
+        | Format-IndentText @splatDent -Depth (0 + $curDepth)
+
+        if ($InputObject.count -gt 1) {
+            $InputObject[0].GetType().Name
+            | Join-String -op $Str.PrefixFirst
+            | Format-IndentText @splatDent -Depth (1 + $curDepth)
+
+            @(
+                $InputObject[0].PSTypeNames
+                | ShortName
+                | Join-String -sep ', '
+            ) | ForEach-Object toString
+            | Join-String -op $Str.PrefixPSTypeNames
+            | Format-IndentText @splatDent -Depth (1 + $curDepth)
+
+        }
+    }
+}
+
+
+if ( ! $experimentToExport ) {
+    hr 2
+    , (Get-ChildItem .) | objTypes_oneline
+
+    hr 2
 }
