170f770e570e3277960488af556438889e12c7f1
Jake Bolton <ninmonkeys@gmail.com>
2022-03-21 18:10:37 -0500

    Update CountIt_basic.ps1
    removed extra

--- public_experiment/CountIt_basic.ps1
+++ public_experiment/CountIt_basic.ps1
@@ -21,13 +21,16 @@ function Measure-ObjectCount {
        This is for cases where you had to use
        ... | Measure-Object | % Count | ...
     .notes
-        -
+        future:
+            - Parameter <condition> that works how Sort-Object's -Property param works
     .example
-        🐒> ls . | Count
+        🐒> ls . | Count # how many files?
+        4
+
     .outputs
           [int]
     .link
-        Dev.Nin\Measure-ObjectCount
+        Dev.Nin\Where-IsNotBlank
     .link
         Dev.Nin\Measure-ObjectCount
 
@@ -40,104 +43,26 @@ function Measure-ObjectCount {
         [Parameter(Mandatory, ValueFromPipeline)]
         [object[]]$InputObject,
 
-        # # instead write count to write-information, pipe object normally
-        # [Parameter()][switch]$PassThru,
-
-        # # do not count 'Blank' values
+        # do not count 'Blank' values
         [Alias('IgnoreNull')]
         [Parameter()][switch]$IgnoreBlank
 
     )
     begin {
-        [hashtalbe]$Config = @{
-            Experimental_AutoEnableEnableWIPref = $false
-        }
-        $original_infaPref = $InformationPreference
-
-        if ($PassThru -or $Label) {
-            if ($Config['Experimental_AutoEnableEnableWIPref']) {
-                $InformationPreference = 'Continue'
-            }
-        }
-        $objectList = [List[object]]::new() # maybe redundant now?
-        switch ($PassThru) {
-            $true {
-
-            }
-            default {
-                [int]$_totalItems = 0
-            }
-
-        }
-        $Config = Join-Hashtable $Config ($Options ?? @{})
+        $objectList = [List[object]]::new()
     }
     process {
-        switch ($PassThru) {
-            $true {
-                $_totalItems++
-                $InputObject
-
-                if (! $Config.PrintOnEveryObject) {
-                    return
-                }
-                $msg = '{0}: {1}' -f @(
-                    $Label ?? 'len'
-                    $objectList.Count
-                )
-
-                if ($Config.PrintNewElementType) {
-                    $msg += ' [{0}]' -f @(
-                        $InputObject
-                        | Dev.Nin\Get-WhatIsShortTypeName
-                        | write-color 'gray45'
-                    )
-
-                }
-                $msg | Write-Information
-
-            }
-            default {
-                $InputObject | ForEach-Object {
-                    $objectList.Add( $_ )
-                }
-            }
-
-        }
+        $objectList.AddRange( $InputObject )
 
     }
     end {
-        if ($PassThru -or $Label) {
-            # can't assume reaches end, or can I reset value on IDisposal/destructor
-            if ($Config['Experimental_AutoEnableEnableWIPref']) {
-                $InformationPreference = $original_infaPref
-            }
+        if ($IgnoreBlank) {
+            $objectList
+            | Dev.Nin\Where-IsNotBlank
+            | Measure-Object | ForEach-Object Count
+        } else {
+            $objectList
+            | Measure-Object | ForEach-Object Count
         }
-        switch ($PassThru) {
-            $true {
-                'Len: {0}' -f $_totalItems
-                #   | Write-Information
-              | Write-Information
-                return
-            }
-            default {
-                if ( $IgnoreBlank) {
-                    $objectList
-                    | Dev.Nin\Where-IsNotBlank
-                    | Measure-Object | ForEach-Object Count
-                    return
-                }
-                $objectList
-                | Measure-Object | ForEach-Object Count
-            }
-
-        }
-
     }
-}
-
-
-if (! $experimentToExport) {
-    3, 4.5, (Get-Item .) , (Get-Date), $null, 50 | len -PassThru -InformationAction Ignore
-    | Out-Null
-    # ...
 }
